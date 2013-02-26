cff5698659191c3634a96a9934e81b0f866b68ba
dzach <dzach@myserver.local>
2013-02-26 02:22:33 +0200

    Fixed yet another special Unicode symbol in both nrfmon.tcl and nrfmon Linux binary

--- nrfmon.tcl
+++ nrfmon.tcl
@@ -23,6 +23,7 @@
 #	►\u25BA	prompt
 #	◄\u25C4	
 #	ϟ\u03DF	user action
+#	Δ\u0394
 #
 
 proc ::init {} {
@@ -2559,7 +2560,7 @@ proc xcvrData {} {
 				R {desc {Rate setting (R)} lsb 0 type scalar from 0 to 127 incr 1 units {} def 35 format %0.0f width 4 deps {DRC,BR DRC,Cs}}
 				Cs {desc {Prescaler ÷8 (Cs)} lsb 7 type boolean opts {0 1} units {} def 0 }
 				BR {desc {Bit Rate (BR)} type entry from 0.337 to 344.827 incr 1.0 incr 0.5 units kbps def 9.5790 format %0.3f  deps {DRC,R DRC,Cs}}
-				DBR {desc {ΔBR} deps {} type function from 0.0 to 100.0 format %0.3f units kbps def 0 format %0.3f}
+				DBR {desc \u0394BR deps {} type function from 0.0 to 100.0 format %0.3f units kbps def 0 format %0.3f}
 			}
 			TXC {
 				desc {11. Transmitter Config.}
